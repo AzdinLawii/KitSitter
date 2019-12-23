b87b5ab279e1bcb946a1385d286e1b3e7b2942f9
frederic-br <frederic.brisoux@gmail.com>
2019-12-23 14:34:48 +0100

    20191223FB: test + troisièmes retouches nav

--- frontend/kitsitter/src/app/nav/nav.component.ts
+++ frontend/kitsitter/src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  toggleMenu() {
+    const NAV = document.getElementsByTagName('nav')[0];
+    NAV.classList.toggle('collapsed');
+    if (NAV.classList.contains('collapsed')) {
+      document.getElementsByTagName('footer')[0].style.marginLeft = 'var(--nv-min-width)';
+      document.getElementsByTagName('footer')[0].style.width = 'calc(100vw - var(--nv-min-width))';
+      document.getElementsByTagName('main')[0].style.marginLeft = 'var(--nv-min-width)';
+      document.getElementsByTagName('main')[0].style.width = 'calc(100vw - var(--nv-min-width))';
+    } else {
+      document.getElementsByTagName('footer')[0].style.marginLeft = 'var(--nv-width)';
+      document.getElementsByTagName('footer')[0].style.width = 'calc(100vw - var(--nv-width))';
+      document.getElementsByTagName('main')[0].style.marginLeft = 'var(--nv-width)';
+      document.getElementsByTagName('main')[0].style.width = 'calc(100vw - var(--nv-width))';
+    }
+  }
 }
