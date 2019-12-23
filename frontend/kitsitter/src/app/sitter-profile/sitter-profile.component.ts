3aa18638776739e186df2d767ba1751f36409115
AzdinLawii <flv.rousseaux@gmail.com>
2019-12-23 15:39:59 +0100

    sitter profile component

---
+++ frontend/kitsitter/src/app/sitter-profile/sitter-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sitter-profile',
+  templateUrl: './sitter-profile.component.html',
+  styleUrls: ['./sitter-profile.component.scss']
+})
+export class SitterProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
