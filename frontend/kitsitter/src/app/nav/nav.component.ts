72e00fc399ee40acd53173832c47c2b92bcbd609
frederic <frederic.brisoux@gmail.com>
2019-12-28 15:22:40 +0100

    20191228-FB : nav design for validation

--- frontend/kitsitter/src/app/nav/nav.component.ts
+++ frontend/kitsitter/src/app/nav/nav.component.ts
@@ -15,17 +15,28 @@ export class NavComponent implements OnInit {
 
   toggleMenu() {
     const NAV = document.getElementsByTagName('nav')[0];
-    NAV.classList.toggle('collapsed');
-    if (NAV.classList.contains('collapsed')) {
-      document.getElementsByTagName('footer')[0].style.marginLeft = 'var(--nv-min-width)';
-      document.getElementsByTagName('footer')[0].style.width = 'calc(100vw - var(--nv-min-width))';
-      document.getElementsByTagName('main')[0].style.marginLeft = 'var(--nv-min-width)';
-      document.getElementsByTagName('main')[0].style.width = 'calc(100vw - var(--nv-min-width))';
+    const FOOTER = document.getElementsByTagName('footer')[0];
+    const MAIN = document.getElementsByTagName('main')[0];
+    if (document.body.getBoundingClientRect().width > 500) {
+      NAV.classList.toggle('collapsed');
+      FOOTER.classList.toggle('collapsed');
+      MAIN.classList.toggle('collapsed');
     } else {
-      document.getElementsByTagName('footer')[0].style.marginLeft = 'var(--nv-width)';
-      document.getElementsByTagName('footer')[0].style.width = 'calc(100vw - var(--nv-width))';
-      document.getElementsByTagName('main')[0].style.marginLeft = 'var(--nv-width)';
-      document.getElementsByTagName('main')[0].style.width = 'calc(100vw - var(--nv-width))';
+      NAV.classList.toggle('expanded');
+    }
+  }
+
+  resizeMenu() {
+    const NAV_LINKS = Array.from(document.getElementsByTagName('nav')[0].getElementsByTagName('a'));
+    const FOOTER = document.getElementsByTagName('footer')[0];
+    if (document.body.getBoundingClientRect().width > 500) {
+      for (const navLink of NAV_LINKS) {
+        navLink.style.display = 'inline-block';
+      }
+    }
+    if (document.body.getBoundingClientRect().width < 801) {
+      FOOTER.style.width = '100%';
+      FOOTER.style.marginLeft = '0';
     }
   }
 }
