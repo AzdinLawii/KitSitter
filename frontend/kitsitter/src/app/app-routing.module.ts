c46b9441daf3d8f6a07d2aaf82b1d8c6eb0ed8e1
Unknown <juliehenriet@gmail.com>
2020-01-13 16:45:58 +0100

    ajout connexion

--- frontend/kitsitter/src/app/app-routing.module.ts
+++ frontend/kitsitter/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {SearchComponent} from './search/search.component';
 import {HomeComponent} from './home/home.component';
 import {CalendarComponent} from './calendar/calendar.component';
 import {CreateinfosComponent} from './createinfos/createinfos.component';
+import {FormulaireComponent} from './formulaire/formulaire.component';
 
 const routes: Routes = [
   {path: '' , component: HomeComponent},
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: 'search', component: SearchComponent },
   {path: 'calendar' , component: CalendarComponent},
   {path: 'createinfos' , component: CreateinfosComponent},
+  {path: 'login' , component:   FormulaireComponent},
 ];
 
 @NgModule({
@@ -19,4 +21,4 @@ const routes: Routes = [
   providers: []
 })
 export class AppRoutingModule { }
-export const routingComponent = [SearchComponent, HomeComponent, CreateinfosComponent];
+export const routingComponent = [SearchComponent, HomeComponent, CreateinfosComponent,   FormulaireComponent];
--- frontend/kitsitter/src/app/formulaire/formulaire.component.ts
+++ frontend/kitsitter/src/app/formulaire/formulaire.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-formulaire',
   templateUrl: './formulaire.component.html',
-  styleUrls: ['./formulaire.component.scss']
+  styleUrls: ['./formulaire.component.css']
 })
 export class FormulaireComponent implements OnInit {
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  resetEyes() {
+    const listeyes = Array.from(document.getElementsByClassName('eye'));
+    listeyes.forEach((eyes: HTMLElement) => {
+      eyes.classList.add('blink');
+      eyes.classList.remove('closed');
+      eyes.style.top = '0';
+    });
+  }
+
+  resetEyesPassword() {
+    this.resetEyes();
+    const listeyes = Array.from(document.getElementsByClassName('eye'));
+    listeyes.forEach((eyes: HTMLElement) => {
+      eyes.classList.remove('blink');
+      eyes.classList.add('closed');
+    });
+  }
+
+  moveEyes(input: HTMLInputElement) {
+    const emailLength = input.value.length;
+    const listLeft = Array.from(document.getElementsByClassName('left'));
+    const listRight = Array.from(document.getElementsByClassName('right'));
+    // 2. If it's the first character:
+    //  2a. move the left pupil to the edge
+    const pupilPos = emailLength / 2.5;
+    if (emailLength === 1) {
+      listLeft.forEach((left: HTMLElement) => {
+        left.style.left = '0';
+      });
+      listRight.forEach((right: HTMLElement) => {
+        right.style.left = '0';
+      });
+    }
+// 3. Move pupil based on email length
+    if (emailLength >= 1 && emailLength <= 36) {
+      listLeft.forEach((left: HTMLElement) => {
+        left.style.left = pupilPos + 'px';
+        left.style.top = '15px';
+      });
+      listRight.forEach((right: HTMLElement) => {
+        right.style.left = pupilPos + 'px';
+        right.style.top = '15px';
+      });
+    }
+  }
 }
+
+
+/**
+
+
+ function resetEyes() {
+    $('.eye').addClass('blink')
+    $('.eye').removeClass('closed')
+
+    if ($('.left').css('left') === '12px') {
+      return
+    }
+
+    $('.inner-eye').css('transform', 'translateY(5px)')
+    $('.left').css('transform', 'translateX(12px)')
+    $('.right').css('transform', 'translateX(-3px)')
+  }
+ });**/
